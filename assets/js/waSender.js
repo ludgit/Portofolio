21ed91dbeb5653405f2a0ad32a1e27898ed42db1
ludgit <149161520+ludgit@users.noreply.github.com>
2024-04-04 23:46:35 +0700

    update

---
+++ assets/js/waSender.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('send-btn').addEventListener('click', function(event) {
+      event.preventDefault();
+      // Ambil nilai dari formulir kontak
+      let name = document.getElementById('name').value;
+      let email = document.getElementById('email').value;
+      let message = document.getElementById('message').value;
+
+      // Buat pesan WhatsApp dengan mencantumkan nama, email, dan pesan dari formulir kontak
+      let whatsappMessage = `Halo, saya tertarik dengan layanan Anda.\n Nama: ${name}\n Email: ${email}\n\n Pesan: ${message}`;
+
+      // Kirim pesan melalui WhatsApp
+      let phoneNumber = '6281331720264';
+      let whatsappURL = `https://api.whatsapp.com/send/?phone=${phoneNumber}&text=${encodeURIComponent(whatsappMessage)}`;
+      window.open(whatsappURL, '_blank');
+
+      // Kirim email (jika Anda ingin tetap menggunakannya)
+      // KirimEmail(name, email, message);
+
+      // Reset formulir (jika diperlukan)
+      document.getElementById('contact-form').reset();
+  });
+});
--- mailer.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'baratzstore1@gmail.com',
-        pass: 'Covid-19'
-    }
-});
